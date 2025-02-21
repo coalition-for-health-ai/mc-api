85a96702574efd1e7f063c55659f84d2ff565961
Sumanth Ratna <sumanth@chai.org>
2025-02-20 23:42:20 -0500

    Use Roboto font

--- src/main/java/org/chai/mc/RendererV01.java
+++ src/main/java/org/chai/mc/RendererV01.java
@@ -394,10 +394,14 @@ public class RendererV01 implements org.chai.mc.Renderer {
                                     <meta charset="UTF-8">
                                     <meta name="viewport" content="width=device-width, initial-scale=1.0">
                                     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/modern-normalize@3.0.1/modern-normalize.min.css">
+                                    <link rel="preconnect" href="https://fonts.googleapis.com">
+                                    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+                                    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
                                     <style>
                                         :root {
                                             print-color-adjust: exact;
                                             -webkit-print-color-adjust: exact;
+                                            font-family: "Roboto", system-ui, sans-serif;
                                             font-optical-sizing: auto;
                                             font-style: normal;
                                             word-wrap: break-word;
